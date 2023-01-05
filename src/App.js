bf366582f66f1846d68be41b954050c9e275d8d3
Nistha <nisthav8@gmail.com>
2023-01-05 19:51:22 +0530

    api fetched and buttons added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const a = [1, 2, 3, 4, "satyam"];
+  const [userData, setuserData] = useState()
+  const handleClick = async(e) => {
+   // alert(e.target.innerText);
+    const id = e.target.innerText;
+    const url = "https://reqres.in/api/users/"+id;
+    const data = await fetch(url);
+    const json = await data.json();
+    setuserData(json.data);
+    //console.log(json.data);
+  };
+  const getData = async () => {
+    const url = "https://reqres.in/api/users?page=1";
+    const data = await fetch(url);
+    const json = await data.json();
+    setData(json.total);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Array(data)
+        .fill(0)
+        .map((_, i) => (
+          <button onClick={handleClick} >{i + 1}</button>
+        ))}
+       {JSON.stringify(userData)}
     </div>
   );
 }
